4e6424ff448782f3196312ad719e780b0cbcc79d
wolffd <wolffdy0@gmail.com>
2019-06-09 18:32:12 +0200

    switching bottom row keys; renaming layers appropriately

--- kb/planck/keymap.c
+++ kb/planck/keymap.c
@@ -21,8 +21,8 @@ extern keymap_config_t keymap_config;
 
 enum planck_layers {
   _QWERTY,
-  _COLEMAK,
-  _DVORAK,
+  _NUMBER,
+  _SYMBOL,
   _NAV,
   _RAISE,
   _PLOVER,
@@ -31,8 +31,8 @@ enum planck_layers {
 
 enum planck_keycodes {
   QWERTY = SAFE_RANGE,
-  COLEMAK,
-  DVORAK,
+  NUMBER,
+  SYMBOL,
   PLOVER,
   BACKLIT,
   EXT_PLV
@@ -45,20 +45,21 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 /* Qwerty
  * ,-----------------------------------------------------------------------------------------------------------------------.
- * | Tab         |   Q  |   W     |   E     |   R    |   T   |   Y   |    U   |   I     |   O     |     P      | Bksp      |
+ * | Tab         |   Q  |   W     |   E     |   R     |   T   |   Y   |    U   |   I     |   O     |     P      | Bksp     |
  * |-----------------------------------------------------------------------------------------------------------------------|
- * | Ctrl / Esc  |   A  |   S     |   D     |   F    |   G   |   H   |    J   |   K     |   L     |   Nav / ;  | Ctrl /  " |
+ * | Ctrl / Esc  |   A  |   S     |   D     |   F     |   G   |   H   |    J   |   K     |   L     |   Nav / ;  | Ctrl / " |
  * |-----------------------------------------------------------------------------------------------------------------------|
- * | Shift / (   |   Z  |   X     |   C     |   V    |   B   |   N   |    M   |   ,     |   .     |      /     | Shift / ) |
+ * | Shift / (   |   Z  |   X     |   C     |   V     |   B   |   N   |    M   |   ,     |   .     |      /     | Shift / )|
  * |-----------------------------------------------------------------------------------------------------------------------|
- * | Brite       |   {  | Alt / [ | GUI / { | Number | Space | Enter | Symbol | GUI / } | ALT / ] |      }     | Right     |
+ * | Brite       |      |   {     | Alt / [ | GUI / { | NUM / | SYM / | GUI / }| ALT / ] |      }  |            |  Right   |
+ * |             |      |         |         |         | Space | Enter |        |         |         |            |          |
  * `-----------------------------------------------------------------------------------------------------------------------'
  */
 [_QWERTY] = LAYOUT_planck_grid(
     KC_TAB,                KC_Q,    KC_W,                KC_E,            KC_R,         KC_T,    KC_Y,    KC_U,          KC_I,            KC_O,                KC_P,                KC_BSPC,
     LCTL_T(KC_ESC),        KC_A,    KC_S,                KC_D,            KC_F,         KC_G,    KC_H,    KC_J,          KC_K,            KC_L,                LT(_NAV, KC_SCLN), RCTL_T(KC_QUOT),
     KC_LSPO, KC_Z,    KC_X,                KC_C,            KC_V,         KC_B,    KC_N,    KC_M,          KC_COMM,         KC_DOT,              KC_SLSH,             KC_RSPC,
-    BACKLIT,               KC_LCBR, LALT_T(KC_LBRACKET), LGUI_T(KC_LCBR), MO(_COLEMAK), KC_SPC,  KC_ENT,  MO(_DVORAK),   RGUI_T(KC_RCBR), RALT_T(KC_RBRACKET), KC_RCBR,             KC_RGHT
+    BACKLIT,  KC_NO,  KC_LCBR, LALT_T(KC_LBRACKET), LGUI_T(KC_LCBR), LT(_NUMBER, KC_SPC),  LT(_SYMBOL, KC_ENT), RGUI_T(KC_RCBR), RALT_T(KC_RBRACKET), KC_RCBR,     KC_NO,   KC_RGHT
 ),
 
 /* Number
@@ -69,14 +70,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |----------------------------------------------------------------------------------------------------------------------|
  * | Shift / (   |      |         |         |        |       |       |        |         |         |           | Shift / ) |
  * |----------------------------------------------------------------------------------------------------------------------|
- * | Brite       |   {  | Alt / [ | GUI / { | Number | Space | Enter | Symbol | GUI / } | ALT / ] |      }    | Right     |
+ * | Brite       |      |   {     | Alt / [ |GUI / { | NUM / | SYM / | GUI / }| ALT / ] |      }  |           |  Right    |
+ * |             |      |         |         |        | Space | Enter |        |         |         |           |           |
  * `----------------------------------------------------------------------------------------------------------------------'
  */
-[_COLEMAK] = LAYOUT_planck_grid(
+[_NUMBER] = LAYOUT_planck_grid(
     KC_TAB,                KC_F1,   KC_F2,               KC_F3,           KC_F4,        KC_F5,   KC_F6,   KC_F7,       KC_F8,           KC_F9,               KC_F10,  KC_BSPC,
     LCTL_T(KC_ESC),        KC_1,    KC_2,                KC_3,            KC_4,         KC_5,    KC_6,    KC_7,        KC_8,            KC_9,                KC_0,    RCTL_T(KC_QUOT),
     KC_LSPO, _______, _______,             _______,         _______,      _______, _______, _______,     _______,         _______,             _______, KC_RSPC,
-    BACKLIT,               KC_LCBR, LALT_T(KC_LBRACKET), LGUI_T(KC_LCBR), MO(_COLEMAK), KC_SPC,  KC_ENT,  MO(_DVORAK), RGUI_T(KC_RCBR), RALT_T(KC_RBRACKET), KC_RCBR, KC_RGHT
+    BACKLIT,  KC_NO,  KC_LCBR, LALT_T(KC_LBRACKET), LGUI_T(KC_LCBR), LT(_NUMBER, KC_SPC),  LT(_SYMBOL, KC_ENT), RGUI_T(KC_RCBR), RALT_T(KC_RBRACKET), KC_RCBR,     KC_NO,   KC_RGHT
 ),
 
 /* Symbol
@@ -87,14 +89,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |----------------------------------------------------------------------------------------------------------------------|
  * | Shift / (   |   ~  |    `    |         |    _   |       |   +   |    -   |    =    |    \    |     |     | Shift / ) |
  * |----------------------------------------------------------------------------------------------------------------------|
- * | Brite       |   {  | Alt / [ | GUI / { | Number | Space | Enter | Symbol | GUI / } | ALT / ] |      }    | Right     |
+ * | Brite       |      |   {     | Alt / [ |GUI / { | NUM / | SYM / | GUI / }| ALT / ] |      }  |           |  Right    |
+ * |             |      |         |         |        | Space | Enter |        |         |         |           |           |
  * `----------------------------------------------------------------------------------------------------------------------'
  */
-[_DVORAK] = LAYOUT_planck_grid(
+[_SYMBOL] = LAYOUT_planck_grid(
     KC_TAB,                KC_F11,  KC_F12,              KC_F13,          KC_F14,       KC_F15,  KC_F16,  KC_F17,      KC_F18,          KC_F19,              KC_F20,  KC_BSPC,
     LCTL_T(KC_ESC),        KC_EXLM, KC_AT,               KC_HASH,         KC_DLR,       KC_PERC, KC_CIRC, KC_AMPR,     KC_ASTR,         KC_QUOT,             KC_DQT,  RCTL_T(KC_QUOT),
     KC_LSPO, KC_TILD, KC_GRV,              _______,         KC_UNDS,      _______, KC_PLUS, KC_MINUS,    KC_EQUAL,        KC_BSLASH,           KC_PIPE, KC_RSPC,
-    BACKLIT,               KC_LCBR, LALT_T(KC_LBRACKET), LGUI_T(KC_LCBR), _______,      KC_SPC,  KC_ENT,  KC_TRNS,     RGUI_T(KC_RCBR), RALT_T(KC_RBRACKET), KC_RCBR, KC_RGHT
+    BACKLIT,  KC_NO,  KC_LCBR, LALT_T(KC_LBRACKET), LGUI_T(KC_LCBR), LT(_NUMBER, KC_SPC),  LT(_SYMBOL, KC_ENT), RGUI_T(KC_RCBR), RALT_T(KC_RBRACKET), KC_RCBR,     KC_NO,   KC_RGHT
 ),
 
 /* Nav
@@ -164,7 +167,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 [_ADJUST] = LAYOUT_planck_grid(
     _______, RESET,   DEBUG,   RGB_TOG, RGB_MOD, RGB_HUI, RGB_HUD, RGB_SAI, RGB_SAD,  RGB_VAI, RGB_VAD, KC_DEL ,
-    _______, _______, MU_MOD,  AU_ON,   AU_OFF,  AG_NORM, AG_SWAP, QWERTY,  COLEMAK,  DVORAK,  PLOVER,  _______,
+    _______, _______, MU_MOD,  AU_ON,   AU_OFF,  AG_NORM, AG_SWAP, QWERTY,  NUMBER,  SYMBOL,  PLOVER,  _______,
     _______, MUV_DE,  MUV_IN,  MU_ON,   MU_OFF,  MI_ON,   MI_OFF,  TERM_ON, TERM_OFF, _______, _______, _______,
     _______, _______, _______, _______, _______, _______, _______, _______, _______,  _______, _______, _______
 )
@@ -189,15 +192,15 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
       }
       return false;
       break;
-    case COLEMAK:
+    case NUMBER:
       if (record->event.pressed) {
-        set_single_persistent_default_layer(_COLEMAK);
+        set_single_persistent_default_layer(_NUMBER);
       }
       return false;
       break;
-    case DVORAK:
+    case SYMBOL:
       if (record->event.pressed) {
-        set_single_persistent_default_layer(_DVORAK);
+        set_single_persistent_default_layer(_SYMBOL);
       }
       return false;
       break;
