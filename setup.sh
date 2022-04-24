42c86ab2f43015441a2f139024521f6b170c2d0d
Dylan Wolff <wolffdy0@gmail.com>
2022-04-24 23:15:13 +0800

    adding script

---
+++ setup.sh
@@ -0,0 +1,23 @@
+!#/bin/bash
+set -e
+sudo apt install -y git vim zsh curl exuberant-ctags tmux python3 zathura python3-pip
+# ssh-keygen
+mkdir -p ~/git
+# git clone git@github.com:dylanjwolff/dotfiles.git ~/git/dotfiles
+cp ~/git/dotfiles/.tmux.conf ~
+cp ~/git/dotfiles/.vimrc ~
+cp ~/git/dotfiles/.zshrc ~
+
+# sudo add-apt-repository ppa:regolith-linux/release
+# sudo apt install regolith-desktop-standard -y
+
+curl -L git.io/antigen > antigen.zsh
+
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+
+chsh -s $(which zsh)
+
+sudo chmod a+w /etc/default/keyboard
+cat /etc/default/keyboard | sed 's/XKBOPTIONS=""/XKBOPTIONS="caps:swapescape"/g' > /etc/default/keyboard
+sudo chmod gu-w /etc/default/keyboard
+
