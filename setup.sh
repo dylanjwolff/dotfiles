309c1b1a399fd3ef41a0684bfb83de11409093fc
Dylan Wolff <wolffdy0@gmail.com>
2022-04-25 22:39:02 +0800

    f2b config

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 !#/bin/bash
 set -e
-sudo apt install -y git vim zsh curl exuberant-ctags tmux python3 zathura python3-pip
+sudo apt install -y git vim zsh curl exuberant-ctags tmux python3 zathura python3-pip openssh-server clang fail2ban mosh
 # ssh-keygen
 mkdir -p ~/git
 # git clone git@github.com:dylanjwolff/dotfiles.git ~/git/dotfiles
@@ -24,3 +24,11 @@ sudo chmod gu-w /etc/default/keyboard
 git config --global user.email "wolffdy0@gmail.com"
 git config --global user.name "Dylan Wolff"
 git config --global core.editor "vim"
+
+sudo systemctl enable ssh
+echo "PasswordAuthentication no" >> /etc/ssh/sshd_config
+
+sudo cp /etc/fail2ban/fail2ban.conf /etc/fail2ban/fail2ban.local
+sudo cp /etc/fail2ban/jail.conf /etc/fail2ban/jail.local
+cp ~/git/dotfiles/jail.local /etc/fail2ban/jail.local
+sudo systemctl enable fail2ban
